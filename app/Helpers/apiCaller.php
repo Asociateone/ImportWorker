0207e997a2ab8e9ba9c94020d272b2f3b06eb15b
Rainaldo Dijkman <61870393+Asociateone@users.noreply.github.com>
2022-09-14 21:07:21 +0200

    added env package so it would be easyer to keep sensitive data safe :)

--- app/Helpers/apiCaller.php
+++ app/Helpers/apiCaller.php
@@ -2,11 +2,14 @@
 
 namespace App\Helpers;
 
+use Dotenv\Dotenv;
+
 class apiCaller
 {
     private string $url;
     private string $client_id;
     private string $token;
+
 
     /**
      * @return void
@@ -15,11 +18,16 @@ class apiCaller
     {
         $this->url = $url;
         $this->client_id = $client_id;
+
+        //load in env file
+        $dotenv = Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
     }
 
     public function call()
     {
         $ch = curl_init("https://id.twitch.tv/oauth2/");
+        getenv("COMPOSER");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $output = curl_exec($ch);
         echo $output;
