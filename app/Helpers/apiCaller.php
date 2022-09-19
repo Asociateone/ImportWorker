f02bec34b77de8b19d03dea42f633569508be746
Rainaldo Dijkman <61870393+Asociateone@users.noreply.github.com>
2022-09-19 21:40:03 +0200

    added dynamic $_env so it would be easyer to reuse this :)

--- app/Helpers/apiCaller.php
+++ app/Helpers/apiCaller.php
@@ -2,45 +2,50 @@
 
 namespace App\Helpers;
 
+use Exception;
 use GuzzleHttp\Client;
-use Symfony\Component\Dotenv\Dotenv;
+use GuzzleHttp\Exception\GuzzleException;
 
 class apiCaller
 {
     private string $url;
     private string $client_id;
-    private string $token;
+    private string $secret_client_id;
+    private string $grant_type;
 
     /**
      * @return void
      */
-    public function __construct(string $url, string $client_id)
+    public function __construct(string $url, $env)
     {
         $this->url = $url;
-        $this->client_id = $client_id;
-
-        $dotenv = new Dotenv();
-        $dotenv->loadEnv('./.env');
+        $this->client_id = $env['AMAZON_CLIENT_ID'];
+        $this->secret_client_id = $env['AMAZON_SECRET_CLIENT_ID'];
+        $this->grant_type = $env['GRANT_TYPE'];
     }
 
+    /**
+     * @param string $link
+     * @return int|void
+     * @throws GuzzleException
+     */
     public function call(string $link)
     {
         $client = new Client([
             'base_uri' => $this->url
         ]);
 
-
         try {
             $response = $client->request('POST', $link, [
                 'form_params' => [
-                    'client_id' => $_ENV['AMAZON_CLIENT_ID'],
-                    'client_secret' => $_ENV['AMAZON_SECRET_CLIENT_ID'],
-                    'grant_type' => $_ENV['GRANT_TYPE'],
+                    'client_id' => $this->client_id,
+                    'client_secret' => $this->secret_client_id,
+                    'grant_type' => $this->grant_type
                 ],
             ]);
 
-            print_r($response);
-        } catch (\Exception $e) {
+            return $response->getStatusCode();
+        } catch (Exception $e) {
             echo $e->getMessage();
         }
     }
--- app/Workers/Amazon.php
+++ app/Workers/Amazon.php
@@ -3,12 +3,19 @@
 namespace App\Workers;
 
 use App\Helpers\apiCaller;
+use Symfony\Component\Dotenv\Dotenv;
 
 Class Amazon {
 
+    public function __construct()
+    {
+        $dotenv = new Dotenv();
+        $dotenv->loadEnv('./.env');
+    }
+
     public function apiCall()
     {
-        $test = new apiCaller('https://id.twitch.tv/', 'test');
+        $test = new apiCaller('https://id.twitch.tv/', $_ENV);
 
         $test->call('oauth2/token');
     }
